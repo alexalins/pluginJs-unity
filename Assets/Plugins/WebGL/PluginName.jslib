2c4270426381b14dff55461f3a6428e2e920e643
alexalins <alexa.glinsoo@gmail.com>
2019-03-22 17:53:08 -0300

    criando plugin para ver funções js no unity webGL

---
+++ Assets/Plugins/WebGL/PluginName.jslib
@@ -0,0 +1,13 @@
+var MyPlugin = {
+    save: function()
+    {
+      localStorage.setItem("texto", "Hello, World!");
+    },
+    get: function()
+    {
+      var texto = localStorage.getItem("texto");
+      window.alert(texto);
+      console.log(texto);
+    },
+};
+mergeInto(LibraryManager.library, MyPlugin);
---
+++ Assets/Script/TesteScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class TesteScript : MonoBehaviour {
+
+    [DllImport("__Internal")]
+    private static extern void save();
+
+    [DllImport("__Internal")]
+    private static extern void get();
+
+    void Start() {
+        save();
+        get();
+    }
+}
